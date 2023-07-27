283a1874fce990c8bd9169186eebd03dc4db2ba9
Ben Young <Ben.Young@erg.com>
2023-07-27 12:37:27 -0400

    update script to average factors across 5 years

--- workdocs/occupational_health_TBS.py
+++ workdocs/occupational_health_TBS.py
@@ -1,13 +1,17 @@
 """
 Generate TBS for Occupational Health data
+Huang et al 2023: https://doi.org/10.1021/acs.est.3c00188
+System Approach for Characterizing and Evaluating Factors for
+Occupational Health Impacts Due to Nonfatal Injuries and Illnesses
+for the Use in Life Cycle Assessment
 """
 import pandas as pd
 import flowsa
 
-repo = 'https://github.com/ZhehanHuang/Occupational-health-calculation/raw/main/data/'
-file = 'developed_dataset/Dataset%20of%20direct%20impact%20factors%20of%20occupational%20health%20impacts.xlsx'
+repo = 'https://github.com/ZhehanHuang/Occupational-health-calculation'
+file = 'raw/main/data/developed_dataset/Dataset%20of%20direct%20impact%20factors%20of%20occupational%20health%20impacts.xlsx'
 
-year = '2018'
+years = ['2014', '2015', '2016', '2017', '2018'] # average across 5 years
 
 ## USEEIO TBS format
 cols = [
@@ -29,29 +33,43 @@ cols = [
     ]
 
 
-def generate_OH_totals(file, year):
-    df = (pd.read_excel(file, sheet_name=year)
-          .filter(['USEEIO Code', 'Total Impact'])
-          .assign(Sector = lambda x: x['USEEIO Code'].apply(str))
-          .rename(columns={'Total Impact': 'FlowAmount'})
-          .assign(Unit = 'DALY / $')
-          .assign(Year = year)
-          .assign(Flowable = 'Injury and illness')
-          .assign(MetaSources = 'TBD')
-          .assign(Location = 'US'))
+def generate_OH_totals(file, years):
+    df_total = pd.DataFrame()
+    for year in years:
+        df = (pd.read_excel(file, sheet_name=year)
+              .filter(['USEEIO Code', 'Total Impact'])
+              .assign(Sector = lambda x: x['USEEIO Code'].apply(str))
+              .rename(columns={'Total Impact': year})
+              .assign(Unit = 'DALY / $ output')
+              # .assign(Year = year)
+              .assign(Flowable = 'Injury and illness')
+              .assign(Context = '')
+              .assign(MetaSources = 'Huang et al. 2023')
+              .assign(Location = 'US'))
 
-    fba = (flowsa.getFlowByActivity('BEA_GDP_GrossOutput', year)
-           .filter(['FlowAmount', 'ActivityProducedBy'])
-           .rename(columns={'ActivityProducedBy': 'Sector',
-                            'FlowAmount': 'Output'})
-           )
+        fba = (flowsa.getFlowByActivity('BEA_GDP_GrossOutput', year)
+               .filter(['FlowAmount', 'ActivityProducedBy'])
+               .rename(columns={'ActivityProducedBy': 'Sector',
+                                'FlowAmount': 'Output'})
+               )
 
-    ## Multiple OH data (coefficients) by industry output
-    df2 = (df.merge(fba, on='Sector')
-           .assign(FlowAmount = lambda x: x['FlowAmount']*x['Output'])
-           .assign(Unit = 'DALY')
-           )
-    return df2.reindex(columns=cols)
+        ## Multiply OH data (coefficients) by industry output
+        df2 = (df.merge(fba, on='Sector')
+               .assign(FlowAmount = lambda x: x[year]*x['Output'])
+               .drop(columns=[year, 'Output', 'USEEIO Code'])
+               .rename(columns={'FlowAmount': year})
+               .assign(Unit = 'DALY')
+               )
+        if len(df_total) == 0:
+            df_total = df2.copy()
+        else:
+            df_total = pd.concat([df_total, df2[year]], axis=1)
 
-df_illness = generate_OH_totals(file=f'{repo}/{file}', year=year)
-# df_illness.to_csv('')
+    # Average across all years
+    df_total['FlowAmount'] = df_total[years].mean(axis=1)
+    df_total = df_total.assign(Year=years[-1])
+
+    return df_total.reindex(columns=cols)
+
+df_illness = generate_OH_totals(file=f'{repo}/{file}', years=years)
+df_illness.to_csv('Huang_Occupational_Health_illness.csv', index=False)
